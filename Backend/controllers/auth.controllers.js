b5865367491b84c6319914bbb4d0c1ed0b6bcefc
Bharath Bolloju <22tq1a6659@siddhartha.co.in>
2025-09-14 20:58:46 +0530

    cookie issue resolved

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -50,7 +50,7 @@ const registerUSer = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -93,7 +93,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
